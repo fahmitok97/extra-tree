bdcda431dba1f1d77e18d42f71872b83cedba00b
fahmi_tok <fahmitok123@gmail.com>
2018-04-15 15:15:42 +0700

    setup project structure

---
+++ classifier/ensemble.py
@@ -0,0 +1,2 @@
+class __Ensemble():
+	pass
---
+++ classifier/tree.py
@@ -0,0 +1,12 @@
+def __entropy():
+	pass
+
+def __score():
+	pass
+
+def __random_split():
+	pass
+
+class __ExtraTreeClassifier():
+	## TODO: core implementation of ETClassifier
+	pass
