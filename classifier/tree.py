315719f497cf30f0ad6160fb1b40b6f466d37318
fahmi_tok <fahmitok123@gmail.com>
2018-04-20 00:38:19 +0700

    add shannon entropy

--- classifier/tree.py
+++ classifier/tree.py
@@ -1,5 +1,20 @@
-def __entropy():
-	pass
+import math
+
+def __entropy(labels):
+	n = len(labels)
+	unique_labels = list(set(labels))
+
+	entropy = 0
+	for label in unique_labels:
+		cnt = 0
+		for elem in labels:
+			if elem == label:
+				cnt += 1
+
+		entropy -= (cnt/n)*math.log2(cnt/n)
+
+	return entropy
+
 
 def __score():
 	pass
